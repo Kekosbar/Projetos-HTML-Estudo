babff2d3adc5c3ec76f882627da6475812369053
Kekosbar <42115781+Kekosbar@users.noreply.github.com>
2019-08-30 16:09:40 -0300

    Add files via upload

---
+++ Project/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $firstName = "Caique";
+        $lastName = "Coelho";
+        $title = "Ms. ";
+        echo "$title $lastName";
+        ?>
+    </body>
+</html>
